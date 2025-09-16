ac82702a853b4f44ce2b97a5e2ebf22a2466eba0
Aurelian <aurelian.drahnea@abc.com>
2025-09-16 11:17:16 +0000

    as

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
 public static void main(String[] args) {
 // Prints "Hello, World" to the terminal window.
 HelloWorldHelper helper = new HelloWorldHelper ();
-System.out.println("Hello, World");
-System.out.println("HOLA");
+System.out.println(helper.useEnglish());
 }
 }
